a6343e37089f6d99770315f2948847f36ee75ec5
YuGyungho <infinite_gh@naver.com>
2018-09-11 11:56:10 +0900

    color_tringle

--- ModernOpenGL/vertexshader.glsl
+++ ModernOpenGL/vertexshader.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 
-layout(location = 0) in vec3 vertexPosition_modelspace;
+in vec3 vertexPosition_modelspace;
+in vec3 vertexColor;
+
+out vec3 fragmentColor;
+
+uniform mat4 MVP;
 
 void main()
 {
-	gl_Position.xyz = vertexPosition_modelspace;
-	gl_Position.w = 1.0;
+	gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+	fragmentColor = vertexColor;
 }
